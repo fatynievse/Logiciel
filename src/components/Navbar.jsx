bf6e8f63b20c97e173b9d355ed7ec774e82512cb
faty <fatimazahra17593@gmail.com>
2024-06-03 14:18:37 +0100

    Updated repository with new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,345 +1,169 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import logo from "../assets/moritlogo.png";
-import {
-  FiArrowRight,
-  FiBarChart2,
-  FiChevronDown,
-  FiHome,
-  FiPieChart,
-} from "react-icons/fi";
-import { AnimatePresence, motion } from "framer-motion";
-// const [showMenu, setShowMenu] = useState(false);
-
-// const toggleMenu = () => {
-//   setShowMenu(!showMenu);
-// };
-
-// const closeMenu = () => {
-// };
-
+import { FaPhone } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return (
-    <div className=" md:flex items-center justify-between h-16 w-full bg-white p-8 text-neutral-200 z-20">
-      <div className="flex-shrink-0">
-        <img src={logo} className="w-32 h-10" alt="Logo" />
-      </div>
-      <div className="flex flex-grow justify-center items-center">
-        <Tabs />
-      </div>
-    </div>
-  );
-};
+  const [showMenu, setShowMenu] = useState(false);
 
-const Tabs = () => {
-  const [selected, setSelected] = useState(null);
-  const [dir, setDir] = useState(null);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
-  const handleSetSelected = (val) => {
-    if (typeof selected === "number" && typeof val === "number") {
-      setDir(selected > val ? "r" : "l");
-    } else if (val === null) {
-      setDir(null);
-    }
-
-    setSelected(val);
+  const closeMenu = () => {
   };
 
   return (
-    <div
-      onMouseLeave={() => handleSetSelected(null)}
-      className="relative flex h-fit gap-2"
-    >
-      {TABS.map((t) => {
-        return (
-          <Tab
-            key={t.id}
-            selected={selected}
-            handleSetSelected={handleSetSelected}
-            tab={t.id}
-          >
-            {t.title}
-          </Tab>
-        );
-      })}
-
-      <AnimatePresence>
-        {selected && selected !== 1 && selected !== 4 && (
-          <Content dir={dir} selected={selected} />
-        )}
-      </AnimatePresence>
-    </div>
-  );
-};
-
-const Tab = ({ children, tab, handleSetSelected, selected }) => {
-  const isSpecialTab = tab === 1 || tab === 4;
-
-  return (
-    <button
-      id={`shift-tab-${tab}`}
-      onMouseEnter={() => handleSetSelected(tab)}
-      onClick={() => handleSetSelected(tab)}
-      className={`flex items-center gap-1 rounded-full px-3 py-1.5 text-sm transition-colors ${
-        selected === tab
-          ? isSpecialTab
-            ? "text-white bg-black"
-            : "bg-neutral-800 text-neutral-100"
-          : "text-neutral-400"
-      }`}
-    >
-      <span>{children}</span>
-      {!isSpecialTab && (
-        <FiChevronDown
-          className={`transition-transform ${
-            selected === tab ? "rotate-180" : ""
-          }`}
-        />
-      )}
-    </button>
-  );
-};
-
-const Content = ({ selected, dir }) => {
-  return (
-    <motion.div
-      id="overlay-content"
-      initial={{
-        opacity: 0,
-        y: 8,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-      }}
-      exit={{
-        opacity: 0,
-        y: 8,
-      }}
-      className="absolute left-0 top-[calc(100%_+_24px)] w-full rounded-lg border border-neutral-600 bg-gradient-to-b from-blue-900 via-blue-900 to-blue-800 p-4"
-    >
-      <Bridge />
-      <Nub selected={selected} />
-
-      {TABS.map((t) => {
-        return (
-          <div className="overflow-hidden" key={t.id}>
-            {selected === t.id && (
-              <motion.div
-                initial={{
-                  opacity: 0,
-                  x: dir === "l" ? 100 : dir === "r" ? -100 : 0,
-                }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.25, ease: "easeInOut" }}
-              >
-                <t.Component />
-              </motion.div>
-            )}
+    <header className="bg-[#f0efef] w-screen h-20 z-20">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="md:flex md:items-center md:gap-12">
+            <Link className="block text-teal-600" to="/">
+              <img src={logo} alt="Logo" className="h-14 w-40" />
+            </Link>
           </div>
-        );
-      })}
-    </motion.div>
-  );
-};
 
-const Bridge = () => (
-  <div className="absolute -top-[24px] left-0 right-0 h-[24px]" />
-);
+          <div className="hidden md:block pr-10">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-lg transition text-blue-500">
+                <li>
+                  <Link
+                    className="p-2 hover:border-b-red-500/75 hover:border-b-2 "
+                    to="/"
+                  >
+                    Accueil
+                  </Link>
+                </li>
 
-const Nub = ({ selected }) => {
-  const [left, setLeft] = useState(0);
+                <li>
+                  <Link
+                    className=" p-2 hover:border-b-red-500/75 hover:border-b-2  "
+                    to="/services"
+                  >
+                    Nos Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" p-2 hover:border-b-red-500/75 hover:border-b-2  "
+                    to="/nosrealisations"
+                  >
+                    Nos Réalisations
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" p-2 hover:border-b-red-500/75 hover:border-b-2  "
+                    to="/Blog"
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="p-2 hover:border-b-red-500/75 hover:border-b-2   "
+                    to="/ContactPage"
+                  >
+                    Contacts
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="absolute left-[80%] top-[0%]">
+            <div className="absolute top-1 left-0 p-3 hidden lg:block border-r-2 border-gray-500 h-14 ">
+              <FaPhone className="w-10 h-10 text-gray-700" />
+            </div>
+            <div className="absolute top-0 left-16 p-4 hidden lg:block">
+              <p className="text-blue-500">Service Client</p>
+              <p className="text-blue-700 font-bold">+212661316191</p>
+            </div>
+          </div>
 
-  useEffect(() => {
-    moveNub();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selected]);
-
-  const moveNub = () => {
-    if (selected) {
-      const hoveredTab = document.getElementById(`shift-tab-${selected}`);
-      const overlayContent = document.getElementById("overlay-content");
-
-      if (!hoveredTab || !overlayContent) return;
-
-      const tabRect = hoveredTab.getBoundingClientRect();
-      const { left: contentLeft } = overlayContent.getBoundingClientRect();
-
-      const tabCenter = tabRect.left + tabRect.width / 2 - contentLeft;
-
-      setLeft(tabCenter);
-    }
-  };
-
-  return (
-    <motion.span
-      style={{
-        clipPath: "polygon(0 0, 100% 0, 50% 50%, 0% 100%)",
-      }}
-      animate={{ left }}
-      transition={{ duration: 0.25, ease: "easeInOut" }}
-      className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 rounded-tl border border-neutral-600 bg-neutral-900"
-    />
-  );
-};
-
-const NosSolutions = () => {
-  return (
-    <div>
-      <div className="flex gap-4">
-        <div>
-          <h3 className="mb-2 text-sm font-medium">GESTION COMMERCIALE</h3>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            Bookkeeping
-          </a>
-          <a href="#" className="block text-sm text-neutral-400 hover:text-red-100">
-            Invoicing
-          </a>
-        </div>
-        <div>
-          <h3 className="mb-2 text-sm font-medium">GESTION DE PAIE</h3>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            Live Coaching
-          </a>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            Reviews
-          </a>
-          <a href="#" className="block text-sm text-neutral-400 hover:text-red-100">
-            Tax/VAT
-          </a>
-        </div>
-        <div>
-          <h3 className="mb-2 text-sm font-medium">COMPTABILITÉ GÉNÉRALE</h3>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            White glove
-          </a>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            SOX Compliance
-          </a>
-          <a href="#" className="block text-sm text-neutral-400 hover:text-red-100">
-            Staffing
-          </a>
-          <a href="#" className="block text-sm text-neutral-400 hover:text-red-100">
-            More
-          </a>
-        </div>
-        <div>
-          <h3 className="mb-2 text-sm font-medium">SOLUTION WEB</h3>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            White glove
-          </a>
-          <a href="#" className="mb-1 block text-sm text-neutral-400 hover:text-red-100">
-            SOX Compliance
-          </a>
-          <a href="#" className="block text-sm text-neutral-400 hover:text-red-100">
-            Staffing
-          </a>
-          <a href="#" className="block text-sm text-neutral-400 hover:text-red-100">
-            More
-          </a>
+          <div className="flex items-center gap-4">
+            <div className="block md:hidden">
+              <button
+                className="rounded bg-blue-100 p-2 text-blue-600 transition hover:text-blue-600/75"
+                onClick={toggleMenu}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
-      <button className="ml-auto mt-4 flex items-center gap-1 text-sm text-indigo-300 hover:text-red-100">
-        <span>View more</span>
-        <FiArrowRight />
-      </button>
-    </div>
+      {/* Mobile Menu */}
+      <div
+        className={`fixed inset-0 z-50 bg-blue-100 transition-transform duration-300 transform ${
+          showMenu ? "translate-x-0" : "-translate-x-full"
+        } md:hidden`}
+      >
+        <nav aria-label="Mobile Global" className="flex flex-col  h-full">
+          <button
+            className="self-end mr-4 p-2 rounded-full bg-blue-600 text-white mt-5"
+            onClick={closeMenu}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <ul className="flex flex-col gap-4 justify-center items-center text-lg mt-[40%]">
+            <li>
+              <Link className="p-2 hover:text-red-500" to="Accueil">
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link className="p-2 hover:text-red-500" to="Services">
+                Nos Services
+              </Link>
+            </li>
+
+            <li>
+              <Link className="p-2 hover:text-red-500" to="Logiciels">
+                Nos Logiciels
+              </Link>
+            </li>
+
+            <li>
+              <Link className="p-2 hover:text-red-500" to="Blog">
+                Blog
+              </Link>
+            </li>
+
+            <li>
+              <Link className="p-2 hover:text-red-500" to="Contact">
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
-
-  const Matériel = () => {
-    return (
-      <div className="grid grid-cols-3 gap-4 divide-x divide-neutral-700">
-        <a
-          href="#"
-          className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
-        >
-          <FiHome className="mb-2 text-xl text-indigo-300" />
-          <span className="text-xs">Startup</span>
-        </a>
-        <a
-          href="#"
-          className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
-        >
-          <FiBarChart2 className="mb-2 text-xl text-indigo-300" />
-          <span className="text-xs">Scaleup</span>
-        </a>
-        <a
-          href="#"
-          className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
-        >
-          <FiPieChart className="mb-2 text-xl text-indigo-300" />
-          <span className="text-xs">Enterprise</span>
-        </a>
-      </div>
-    );
-  };
-
-  const Ressources = () => {
-    return (
-      <div>
-        <div className="grid grid-cols-2 gap-2">
-          <a href="#">
-            <img
-              className="mb-2 h-14 w-full rounded object-cover"
-              src="/imgs/blog/4.png"
-              alt="Placeholder image"
-            />
-            <h4 className="mb-0.5 text-sm font-medium">Lorem ipsum dolor</h4>
-            <p className="text-xs text-neutral-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo
-              quidem eos.
-            </p>
-          </a>
-          <a href="#">
-            <img
-              className="mb-2 h-14 w-full rounded object-cover"
-              src="/imgs/blog/5.png"
-              alt="Placeholder image"
-            />
-            <h4 className="mb-0.5 text-sm font-medium">Lorem ipsum dolor</h4>
-            <p className="text-xs text-neutral-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo
-              quidem eos.
-            </p>
-          </a>
-        </div>
-        <button className="ml-auto mt-4 flex items-center gap-1 text-sm text-indigo-300">
-          <span>View more</span>
-          <FiArrowRight />
-        </button>
-      </div>
-    );
-  };
-  const Accueil = () => {
-    return <></>;
-  };
-  const Infogérance = () => {
-    return <></>;
-  };
-  const TABS = [
-    {
-      title: "Accueil",
-      Component: Accueil,
-    },
-    {
-      title: "Nos Solutions",
-      Component: NosSolutions,
-    },
-    {
-      title: "Matériel point de vente",
-      Component: Matériel,
-    },
-    {
-      title: "Infogérance",
-      Component: Infogérance,
-    },
-    {
-      title: "Ressources",
-      Component: Ressources,
-    },
-  ].map((n, idx) => ({ ...n, id: idx + 1 }));
-
-  export default Navbar;
+export default Navbar;
